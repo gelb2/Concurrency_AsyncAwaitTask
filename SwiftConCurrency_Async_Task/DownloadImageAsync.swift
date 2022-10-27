847e5983655d5bae31796a8af92d1a5ddee3a448
JunYoungJee <rainmaker8492@gmail.com>
2022-10-28 04:17:18 +0900

    handle REsponse with helper Function

--- SwiftConCurrency_Async_Task/DownloadImageAsync.swift
+++ SwiftConCurrency_Async_Task/DownloadImageAsync.swift
@@ -25,16 +25,9 @@ class DownloadImageAsyncImageLoader {
     //completionHandler가 downloadWithEscaping 함수 안에서는 쓰일 수 있으나, downloadWithEscaping 밖에선 쓰일 수 없다. 어떻게 해결할까?
     //그럴때 @escaping 을 붙이는 것이다
     func downloadWithEscaping(completionHandler: @escaping (_ image: UIImage?, _ error: Error?) -> ()) {
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            guard let data = data,
-                  let image = UIImage(data: data),
-                  let response = response as? HTTPURLResponse,
-                  response.statusCode >= 200 && response.statusCode < 300 else {
-                completionHandler(nil, error)
-                return
-            }
-            
-            completionHandler(image, nil)
+        URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            let image = self?.handleResponse(data: data, response: response)
+            completionHandler(image, error)
         }
         .resume()
     }
