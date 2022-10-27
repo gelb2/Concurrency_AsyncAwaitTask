1b82f5d1620c89b099e665e1b8ee905e53f70a5a
JunYoungJee <rainmaker8492@gmail.com>
2022-10-28 07:15:55 +0900

    가변적인 배열의 갯수에 따라 withThrowingTaskGroup + For Loop 사용하여 여러번의 async API 호출을 동시에 Concurrent하는 방법 확인

--- SwiftConCurrency_Async_Task/TaskGroupBootcamp.swift
+++ SwiftConCurrency_Async_Task/TaskGroupBootcamp.swift
@@ -21,32 +21,38 @@ class TaskGroupBootcampDataManager {
     
     func fetchIOmagesWithTaskGroup() async throws -> [UIImage] {
         
+        let urlStrings = [
+            "https://picsum.photos/300",
+            "https://picsum.photos/300",
+            "https://picsum.photos/300",
+            "https://picsum.photos/300",
+            "https://picsum.photos/300"
+        ]
+        
         //withThrowningTaskGroup과 withTaskGroup의 차이는 에러 던지기의 여부다
         
         //태스크그룹 안의 차일드태스크 타입을 정해준다. 이미지를 던지는 메소드를 넣을 예정이니 UIImage.self
-        return try await withThrowingTaskGroup(of: UIImage.self) { group in
+        return try await withThrowingTaskGroup(of: UIImage?.self) { group in
+            
             var images: [UIImage] = []
+            //컴파일러에게 정확한 배열 사이즈를 알려주면 최적화에 도움이 된다
+            images.reserveCapacity(urlStrings.count)
             
-            //차일드태스크를 만들어서 그룹에 추가한다
-            //차일드태스크는 패런츠태스크의 프라이오리티를 따라간다. 필요시에 프라이오리티를 수정하면 된다
-            group.addTask {
-                try await self.fetchImage(urlString: "https://picsum.photos/300")
+            for urlString in urlStrings {
+                //차일드태스크를 만들어서 그룹에 추가한다
+                //차일드태스크는 패런츠태스크의 프라이오리티를 따라간다. 필요시에 프라이오리티를 수정하면 된다
+                group.addTask {
+                    //루프로 돌린 리퀘스트중 하나 정도는 실패하더라도 나머지 값은 다 리턴을 받는게 좋으므로 옵셔널로 수정
+                    try? await self.fetchImage(urlString: urlString)
+                }
             }
             
-            group.addTask {
-                try await self.fetchImage(urlString: "https://picsum.photos/300")
-            }
-            
-            group.addTask {
-                try await self.fetchImage(urlString: "https://picsum.photos/300")
-            }
-            
-            group.addTask {
-                try await self.fetchImage(urlString: "https://picsum.photos/300")
-            }
-            
+            //루프가 다 돌아서 group의 갯수만큼 이미지들이 다 만들어지고, 만들어진 이미지들이 다 append되면 return images를 호출
+            //group안에 들어간 차일드태스크들은 모두 동시에 concurrent하게 동시에 호출된다
             for try await image in group {
-                images.append(image)
+                if let image = image {
+                    images.append(image)
+                }
             }
             
             return images
