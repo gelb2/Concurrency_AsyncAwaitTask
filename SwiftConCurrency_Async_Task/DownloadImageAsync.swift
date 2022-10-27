344702436329b36dd57f11cae46622353c095f7a
JunYoungJee <rainmaker8492@gmail.com>
2022-10-28 04:15:23 +0900

    downloadImage with Escaping Closure

---
+++ SwiftConCurrency_Async_Task/DownloadImageAsync.swift
@@ -0,0 +1,80 @@
+//
+//  DownloadImageAsync.swift
+//  SwiftConCurrency_Async_Task
+//
+//  Created by pablo.jee on 2022/10/28.
+//
+
+import SwiftUI
+
+class DownloadImageAsyncImageLoader {
+    
+    let url = URL(string: "https://picsum.photos/200")!
+    
+    func handleResponse(data: Data?, response: URLResponse?) -> UIImage? {
+        guard let data = data,
+              let image = UIImage(data: data),
+              let response = response as? HTTPURLResponse,
+              response.statusCode >= 200 && response.statusCode < 300 else {
+            return nil
+        }
+        
+        return image
+    }
+    
+    //completionHandler가 downloadWithEscaping 함수 안에서는 쓰일 수 있으나, downloadWithEscaping 밖에선 쓰일 수 없다. 어떻게 해결할까?
+    //그럴때 @escaping 을 붙이는 것이다
+    func downloadWithEscaping(completionHandler: @escaping (_ image: UIImage?, _ error: Error?) -> ()) {
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data,
+                  let image = UIImage(data: data),
+                  let response = response as? HTTPURLResponse,
+                  response.statusCode >= 200 && response.statusCode < 300 else {
+                completionHandler(nil, error)
+                return
+            }
+            
+            completionHandler(image, nil)
+        }
+        .resume()
+    }
+}
+
+class DownloadImageAsyncViewModel: ObservableObject {
+    @Published var image: UIImage? = nil
+    let loader = DownloadImageAsyncImageLoader()
+    
+    func fetchImage() {
+        loader.downloadWithEscaping { [weak self] image, error in
+            DispatchQueue.main.async {
+                self?.image = image
+            }
+        }
+    }
+    
+}
+
+struct DownloadImageAsync: View {
+    
+    @StateObject private var viewModel = DownloadImageAsyncViewModel()
+    
+    var body: some View {
+        ZStack {
+            if let image = viewModel.image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+            }
+        }
+        .onAppear {
+            viewModel.fetchImage()
+        }
+    }
+}
+
+struct DownloadImageAsync_Previews: PreviewProvider {
+    static var previews: some View {
+        DownloadImageAsync()
+    }
+}
