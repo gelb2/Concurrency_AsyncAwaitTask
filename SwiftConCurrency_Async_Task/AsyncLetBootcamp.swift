8b31f9d32a1de903be33e2ca5d6a2b3f825d31e0
JunYoungJee <rainmaker8492@gmail.com>
2022-10-28 06:21:37 +0900

    LazyVGrid + Serial Code Calls With Await

---
+++ SwiftConCurrency_Async_Task/AsyncLetBootcamp.swift
@@ -0,0 +1,72 @@
+//
+//  AsyncLetBootcamp.swift
+//  SwiftConCurrency_Async_Task
+//
+//  Created by pablo.jee on 2022/10/28.
+//
+
+import SwiftUI
+
+struct AsyncLetBootcamp: View {
+    
+    @State private var images: [UIImage] = []
+    let columns = [GridItem(.flexible()), GridItem(.flexible())]
+    let url = URL(string: "https://picsum.photos/200")!
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(images, id: \.self) { image in
+                        Image(uiImage: image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 150)
+                    }
+                }
+            }
+            .navigationTitle("async let title")
+            .onAppear {
+                
+                //태스크 안에 있는 fetchImage() 들은 serial하게 불린다...await로 기다리게 하니까...
+                //어떻게 concurrent 하게 할까?
+                Task {
+                    do {
+                        let image1 = try await fetchImage()
+                        self.images.append(image1)
+                        
+                        let image2 = try await fetchImage()
+                        self.images.append(image2)
+                        
+                        let image3 = try await fetchImage()
+                        self.images.append(image3)
+                        
+                        let image4 = try await fetchImage()
+                        self.images.append(image4)
+                    } catch {
+                        
+                    }
+                }
+            }
+        }
+    }
+    
+    func fetchImage() async throws -> UIImage {
+        do {
+           let (data, _) = try await URLSession.shared.data(from: url, delegate: nil)
+            if let image = UIImage(data: data) {
+                return image
+            } else {
+                throw URLError(.badURL)
+            }
+        } catch {
+            throw error
+        }
+    }
+}
+
+struct AsyncLetBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        AsyncLetBootcamp()
+    }
+}
